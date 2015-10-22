91551c1ce6a13246286691f1e983e4a96ccaad5d
Ash Pook <ashleighpook@gmail.com>
2015-10-22 23:15:27 +0100

    A little progress.

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -3,11 +3,52 @@ package hayago
 object Game {
   import scala.util._
   import com.github.nscala_time.time.Imports._
+  import scala.collection.immutable.HashSet
+
+  object Logic {
+    case class Group (stones: HashSet[Board.Intersection]){
+      def liberties: HashSet[Board.Intersection] = ???
+    }
+
+    def aggregate (board: Board, colour: Colour): HashSet[Group] = ???
+
+    // this fn doesn't care about who's move it actually it.
+    // it just takes a board, a point and a colour, and updates the board state according to the rules of go.
+    def applyPlay (board: Board, i: Board.Intersection, colour: Colour): Board = {
+      val enemyGroups = aggregate (board, Colour.opposition (firstTurnColour))
+
+      val toRemove = HashSet() ++ enemyGroups.toList.collect { case enemyGroup if
+        enemyGroup.liberties.contains (i) => enemyGroup.stones.toList
+      }.flatten
+
+      val board2 = board
+        .clear (toRemove)
+        .add (i, colour)
+
+      val friendlyGroups =  aggregate (board2, colour)
+      val toRemove2 = HashSet() ++ friendlyGroups.toList.collect { case friendlyGroup if
+        friendlyGroup.liberties.isEmpty => friendlyGroup.stones.toList
+      }.flatten
+
+      board
+        .clear (toRemove2)
+    }
+  }
 
   final case class Board (private val grid: Matrix[Option[Colour]]) {
-    val size: Int = grid.size
-    def apply (i: Board.Intersection): Try[Option[Colour]] = grid (i.x, i.y)
-    def update (i: Board.Intersection, value: Option[Colour]): Try[Board] = grid.update (i.x, i.y, value).map (Board (_))
+    val size: Int = grid.rowCount
+    // get
+    def apply (i: Board.Intersection): Option[Colour] = grid (i.x, i.y).getOrElse (None)
+    def get (i: Board.Intersection): Option[Colour] = apply (i)
+
+    def add (i: Board.Intersection, colour: Colour): Board = update (i, Some (colour))
+
+    // update
+    def update (i: Board.Intersection, state: Option[Colour]): Board = grid.update (i.x, i.y, state).map (Board (_)).getOrElse (this)
+
+    // clear
+    def clear (i: Board.Intersection): Board = update (i, None)
+    def clear (hs: HashSet[Board.Intersection]): Board = hs.foldLeft (this) { (a, i) => a.clear (i)}
   }
   object Board {
     // counting from zero
@@ -66,32 +107,22 @@ object Game {
 
     def board: Board = {
       val numIntersections = setup.boardSize * setup.boardSize
-      val empty = Board (Matrix[Option[Colour]] (numIntersections))
+      val empty = Board (Matrix[Option[Colour]] (numIntersections, numIntersections))
       val withHandicap = setup.handicap.toList.foldLeft (empty) { (a, i) =>
         val intersection = i._1
         val player = i._2
-        applyPlay (a, intersection, colour (player)) match {
-          case Failure (_) => a
-          case Success (ok) => ok
-        }
+        Logic.applyPlay (a, intersection, colour (player))
       }
       history.indices.foldLeft (withHandicap) { (a, i) =>
         val turn = history (i)
         val colour = colourToPlayTurn (i)
         turn.action match {
           case Left (_) => a
-          case Right (intersection) =>
-            applyPlay (a, intersection, colour) match {
-              case Failure (_) => a
-              case Success (ok) => ok
-            }
+          case Right (intersection) => Logic.applyPlay (a, intersection, colour)
         }
       }
     }
 
-    // this fn doesn't care about who's move it actually it.
-    // it just takes a board, a point and a colour, and updates the board state according to the rules of go.
-    def applyPlay (board: Board, i: Board.Intersection, colour: Colour): Try[Board] = ???
 
     def playerToPlayTurn (index: Int): Player = if (index % 2 == 0) setup.firstTurn else Player.opposition (setup.firstTurn)
     def playerToPlayNext = playerToPlayTurn (history.size)
--- src/main/scala/Matrix.scala
+++ src/main/scala/Matrix.scala
@@ -3,16 +3,18 @@ package hayago
 import scala.util.Try
 
 case class MatrixImpl[T] (private val data: List[T]) extends Matrix[T] {
-  val size: Int = ???
+  val rowCount: Int = ???
+  val columnCount: Int = ???
   def apply (row: Int, column: Int): Try[T] = ???
   def update (row: Int, column: Int, value: T): Try[Matrix[T]] = ???
 }
 
 abstract class Matrix[T] {
-  val size: Int
+  val rowCount: Int
+  val columnCount: Int
   def apply (row: Int, column: Int): Try[T]
   def update (row: Int, column: Int, value: T): Try[Matrix[T]]
 }
 object Matrix {
-  def apply[T] (size: Int): Matrix[T] = ???
+  def apply[T] (rowCount: Int, columnCount: Int): Matrix[T] = ???
 }
