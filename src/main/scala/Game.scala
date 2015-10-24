ee350963cb530f3e6cd1ae488097fb4ef6678b76
Ashleigh Pook <ashleighpook@gmail.com>
2015-10-24 23:48:31 +0100

    Updated Matrix implementation.

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -11,8 +11,9 @@ object Game {
     }
 
     // Aggregate groups of stones on the board for a particular colour.
-    def aggregate (board: Board, colour: Colour): HashSet[Group] = ???
-
+    def aggregate (board: Board, colour: Colour): HashSet[Group] = {
+      ???
+    }
     // Applies a play to the board.
     // this fn doesn't care about who's move it actually it.
     // it just takes a board, a point and a colour, and updates the board state according to the rules of go.
@@ -23,7 +24,7 @@ object Game {
         group.liberties.contains (i) && group.liberties.size <= 1 => group.stones.toList
       }.flatten
       val boardWithPlay = board
-        .clear (oppositionToRemove)
+        .cleared (oppositionToRemove)
         .add (i, colour) // now add the play to the board
       // Perhaps you made a suicide move.  Not sure if doing so is actually illegal.  If it is then this
       // bit can be removed.
@@ -31,24 +32,24 @@ object Game {
       val friendsToRemove = friendlyGroups.toList.collect { case group if
         group.liberties.isEmpty => group.stones.toList
       }.flatten
-      board.clear (friendsToRemove)
+      board.cleared (friendsToRemove)
     }
   }
 
   final case class Board (private val grid: Matrix[Option[Colour]]) {
     val size: Int = grid.rowCount
     // get
-    def apply (i: Board.Intersection): Option[Colour] = grid (i.x, i.y).getOrElse (None)
+    def apply (i: Board.Intersection): Option[Colour] = grid (i.x, i.y)
     def get (i: Board.Intersection): Option[Colour] = apply (i)
 
-    def add (i: Board.Intersection, colour: Colour): Board = update (i, Some (colour))
+    def add (i: Board.Intersection, colour: Colour): Board = updated (i, Some (colour))
 
     // update
-    def update (i: Board.Intersection, state: Option[Colour]): Board = grid.update (i.x, i.y, state).map (Board (_)).getOrElse (this)
+    def updated (i: Board.Intersection, state: Option[Colour]): Board = Board (grid.updated (i.x, i.y, state))
 
     // clear
-    def clear (i: Board.Intersection): Board = update (i, None)
-    def clear (hs: List[Board.Intersection]): Board = hs.foldLeft (this) { (a, i) => a.clear (i) }
+    def cleared (i: Board.Intersection): Board = updated (i, None)
+    def cleared (hs: List[Board.Intersection]): Board = hs.foldLeft (this) { (a, i) => a.cleared (i) }
   }
   object Board {
     // counting from zero
@@ -104,10 +105,10 @@ object Game {
   case class Configuration (boardSize: Int = 19, firstTurn: Player = Montague, handicap: Map[Board.Intersection, Player] = Map (), komi: Float = 6.5f)
 
   case class State (setup: Configuration = Configuration (), history: List[Turn] = Nil) {
-
     def board: Board = {
-      val numIntersections = setup.boardSize * setup.boardSize
-      val empty = Board (Matrix[Option[Colour]] (numIntersections, numIntersections))
+      val n = setup.boardSize * setup.boardSize
+      val grid = Matrix.tabulate[Option[Colour]] (n, n) { (_, _) => None }
+      val empty = Board (grid)
       val withHandicap = setup.handicap.toList.foldLeft (empty) { (a, i) =>
         val intersection = i._1
         val player = i._2
@@ -122,17 +123,12 @@ object Game {
         }
       }
     }
-
-
     def playerToPlayTurn (index: Int): Player = if (index % 2 == 0) setup.firstTurn else Player.opposing (setup.firstTurn)
     def playerToPlayNext = playerToPlayTurn (history.size)
-
     def colourToPlayTurn (index: Int): Colour = if (playerToPlayTurn (index) == setup.firstTurn) firstTurnColour else Colour.opposing (firstTurnColour)
     def colourToPlayNext = colourToPlayTurn (history.size)
-
     def colour (player: Player) = if (player == setup.firstTurn) firstTurnColour else Colour.opposing (firstTurnColour)
     def player (colour: Colour) = if (colour == Black) setup.firstTurn else Player.opposing (setup.firstTurn)
-
     def isTurnLegal (turn: Turn): Boolean = ???
     def isComplete: Boolean = history.reverse match {
       case z if z.contains (Turn.resign) => true
--- src/main/scala/Matrix.scala
+++ src/main/scala/Matrix.scala
@@ -2,19 +2,41 @@ package hayago
 
 import scala.util.Try
 
-case class MatrixImpl[T] (private val data: List[T]) extends Matrix[T] {
-  val rowCount: Int = ???
-  val columnCount: Int = ???
-  def apply (row: Int, column: Int): Try[T] = ???
-  def update (row: Int, column: Int, value: T): Try[Matrix[T]] = ???
-}
-
+/*
+ * An immutable matrix type.
+ */
 abstract class Matrix[T] {
   val rowCount: Int
   val columnCount: Int
-  def apply (row: Int, column: Int): Try[T]
-  def update (row: Int, column: Int, value: T): Try[Matrix[T]]
+  def apply (row: Int, column: Int): T
+  def updated (row: Int, column: Int, value: T): Matrix[T]
 }
 object Matrix {
-  def apply[T] (rowCount: Int, columnCount: Int): Matrix[T] = ???
+  def tabulate[T] (rowCount: Int, columnCount: Int)(f: (Int, Int) => T): Matrix[T] = {
+    if (rowCount <= 0 || columnCount <= 0)
+      throw new NegativeArraySizeException
+    else {
+      val data = (0 until rowCount * columnCount).map { index =>
+        val x = index % rowCount
+        val y = (index - x) / columnCount
+        f (x, y)
+      }.toList
+      MatrixImpl (rowCount, columnCount, data)
+    }
+  }
+}
+
+
+case class MatrixImpl[T] (rowCount: Int, columnCount: Int, private val data: List[T]) extends Matrix[T] {
+  def apply (row: Int, column: Int): T = {
+    if (row < 0 || row >= rowCount || column < 0 || column >= columnCount)
+      throw new IndexOutOfBoundsException
+    else data (row + column * rowCount)
+  }
+  def updated (row: Int, column: Int, value: T): Matrix[T] = {
+    if (row < 0 || row >= rowCount || column < 0 || column >= columnCount)
+      throw new IndexOutOfBoundsException
+    else MatrixImpl (rowCount, columnCount, data.updated (row + column * rowCount, value))
+
+  }
 }
